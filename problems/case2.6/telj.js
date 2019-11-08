afeda295957fae1c11813c047fd1cf63a83e9fc3
wangmeng <magic801@126.com>
2019-11-08 10:56:21 +0800

    Add 38报数

---
+++ problems/case2.6/telj.js
@@ -0,0 +1,34 @@
+let canvas = document.getElementById('mycan')
+
+let stage = new createjs.Stage(canvas)
+createjs.Touch.enable(stage)
+
+let state = {
+  rotate: false
+}
+
+let rect = new createjs.Shape()
+rect.graphics.beginFill('#ccc').drawRect(0,0,40,40)
+rect.regX = rect.regY = 20
+rect.x = rect.y = 100
+
+stage.addChild(rect)
+stage.update()
+
+createjs.Ticker.setFPS(60)
+createjs.Ticker.addEventListener('tick', () => {
+  if (stage.rotate) {
+    rect.rotation++
+    stage.update()
+  }
+})
+
+rect.on('mousedown', (e) => {
+  stage.rotate = !stage.rotate
+})
+
+stage.on('stagemousemove', (e) => {
+  rect.x = e.stageX
+  rect.y = e.stageY
+  stage.update()
+})
