29bc87c2cbc75d98745303f8cdf4d4fa9bcaf853
wangmeng <magic801@126.com>
2020-02-06 22:00:16 +0800

    Add qunee demo

---
+++ problems/case3.0/helloworld/test.js
@@ -0,0 +1,19 @@
+if (!window.getI18NString) {
+    getI18NString = function(s) {
+        return s;
+    }
+}
+
+var graph = new Q.Graph('demon');
+var hello = graph.createNode("Hello", -100, -50);
+hello.image = Q.Graphs.server;
+var qunee = graph.createNode("Qunee", 100, 50);
+var edge = graph.createEdge("Hello\nQunee", qunee, hello);
+
+edge.setStyle(Q.Styles.LABEL_OFFSET_Y, -10);
+edge.setStyle(Q.Styles.LABEL_POSITION, Q.Position.CENTER_TOP);
+edge.setStyle(Q.Styles.LABEL_ANCHOR_POSITION, Q.Position.CENTER_BOTTOM);
+edge.setStyle(Q.Styles.LABEL_BORDER, 1);
+edge.setStyle(Q.Styles.LABEL_POINTER, true);
+edge.setStyle(Q.Styles.LABEL_PADDING, new Q.Insets(2, 5));
+edge.setStyle(Q.Styles.LABEL_BACKGROUND_GRADIENT, Q.Gradient.LINEAR_GRADIENT_VERTICAL);
