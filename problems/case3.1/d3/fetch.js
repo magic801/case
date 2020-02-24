4b6d322aacc573265d9e42fc4c804288206a97df
王猛（wangmeng）-基线研发部 <wangmeng@qiyi.com>
2020-02-24 14:17:54 +0800

    Add 假数据

--- problems/case3.1/d3/fetch.js
+++ problems/case3.1/d3/fetch.js
@@ -35,6 +35,74 @@ let map = {
       { source: 0, target: 3}
     ]
   },
+  '101': {
+    id: '1010',
+    nodes: [{
+      // id: '101',
+      name: '1军'
+    }, {
+      id: '10101',
+      name: '军旗'
+    }, {
+      id: '10102',
+      name: '大炮'
+    }, {
+      id: '10103',
+      name: '步兵'
+    }, {
+      id: '10104',
+      name: '骑兵'
+    }],
+    edges: [
+      { source: 0, target: 1},
+      { source: 0, target: 2},
+      { source: 0, target: 3},
+      { source: 0, target: 4}
+    ]
+  },
+  '102': {
+    id: '1020',
+    nodes: [{
+      // id: '101',
+      name: '2军'
+    }, {
+      id: '10201',
+      name: '军旗'
+    }, {
+      id: '10202',
+      name: '大炮'
+    }, {
+      id: '10203',
+      name: '步兵'
+    }],
+    edges: [
+      { source: 0, target: 1},
+      { source: 0, target: 2},
+      { source: 0, target: 3}
+    ]
+  },
+  '103': {
+    id: '1030',
+    nodes: [{
+      // id: '101',
+      name: '3军'
+    }, {
+      id: '10301',
+      name: '军旗'
+    }, {
+      id: '10302',
+      name: '火箭兵'
+    }, {
+      id: '10303',
+      name: '步兵'
+    }],
+    edges: [
+      { source: 0, target: 1},
+      { source: 0, target: 2},
+      { source: 0, target: 3}
+    ]
+  },
+
   '2': {
     id: '20',
     nodes: [{
@@ -60,31 +128,58 @@ let map = {
       { source: 0, target: 4}
     ]
   },
-  '101': {
-    id: '1010',
+  '201': {
+    id: '2010',
     nodes: [{
-      // id: '101',
-      name: '1军'
+      // id: '1',
+      name: '1师'
     }, {
-      id: '10101',
-      name: '军旗'
-    }, {
-      id: '10102',
-      name: '大炮'
-    }, {
-      id: '10103',
-      name: '步兵'
-    }, {
-      id: '10104',
-      name: '骑兵'
+      id: '201',
+      name: '师长'
     }],
     edges: [
-      { source: 0, target: 1},
-      { source: 0, target: 2},
-      { source: 0, target: 3},
-      { source: 0, target: 4}
+      { source: 0, target: 1}
     ]
-  }
+  },
+  '202': {
+    id: '2020',
+    nodes: [{
+      // id: '1',
+      name: '1师'
+    }, {
+      id: '202',
+      name: '师长'
+    }],
+    edges: [
+      { source: 0, target: 1}
+    ]
+  },
+  '203': {
+    id: '2030',
+    nodes: [{
+      // id: '1',
+      name: '3师'
+    }, {
+      id: '203',
+      name: '师长'
+    }],
+    edges: [
+      { source: 0, target: 1}
+    ]
+  },
+  '204': {
+    id: '2040',
+    nodes: [{
+      // id: '1',
+      name: '师'
+    }, {
+      id: '204',
+      name: '师长'
+    }],
+    edges: [
+      { source: 0, target: 1}
+    ]
+  },
 }
 
 let getInfo = (node) => {
